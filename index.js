f1484217474d39b0d4b9510af1cf11dfc081e4d6
kausik <kkausik11@g,ail.com>
2025-12-23 11:58:18 +0530

    add acknowledge for backend

--- index.js
+++ index.js
@@ -26,24 +26,38 @@ io.on('connection',(socket)=>{
     console.log(message);
   })
 
-   socket.on("chat message",(msg)=>{
-    console.log("a user is connected for message: " + msg);
+   socket.on("chat message",(msg,ack)=>{
+    console.log("a user send a chat message: " + msg);
 
     io.emit('chat message', msg)
+
+    // acknowledgement to sender
+    if (typeof ack === "function") {
+      ack({
+        status: "ok",
+        message: "Message delivered",
+        socketId: socket.id,
+      });
+    }
    })
 
    
 
 
-  socket.on('disconnected', ()=>{
+  socket.on('disconnected', (msg)=>{
     console.log(`${socket.id} is disconnected`);
+    io.emit('disconnected', `${socket.id} is disconnected`);
   })
 
-  socket.on("hello", (a, b, c)=>{
-    console.log(a);
-    console.log(b);
-    
-  })
+
+  // socket.on('hello connect again', (arg1, arg2, arg3)=>{
+  //     console.log("arg1: ", arg1);
+  //     console.log("arg2: ", arg2);
+  //     console.log("arg3: ", arg3);   
+  // })
+
+
+ 
 })
 
 // io.emit('some event', 'hello world')
